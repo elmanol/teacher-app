e6a43def85a99cdfd1582ffe69163a75f1703f63
elmanol <kerimakis@ceid.upatras.gr>
2024-09-22 22:38:09 +0200

    add Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import TeacherList from './components/TeacherList';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Dashboard from './Dashboard';
+import ProTip from './ProTip';
 
-const App: React.FC = () => {
+
+function Copyright() {
   return (
-    <div>
-      <h1>Welcome to the Teacher App</h1>
-      <TeacherList />
-    </div>
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
   );
-};
+}
 
-export default App;
+export default function App() {
+  return (
+    // <Container maxWidth="sm">
+    //   <Box sx={{ my: 4 }}>
+    //     <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+    //       Material UI Vite.js example in TypeScript
+    //     </Typography>
+      <Dashboard />
+    //   </Box>
+    // </Container>
+  );
+}
